a709346fa83d64bb1f7af2315d923e3205a9b54f
Jerry O'Donnell <gerald-odonnell7@users.noreply.github.com>
2014-06-26 21:58:29 -0400

    Create greetings.py

---
+++ greetings.py
@@ -0,0 +1,9 @@
+#raw_input is for strings
+name = raw_input("What is your name?")
+print "Hello and greetings", name
+
+age = input("How old are you?")
+print "You are", age, "years old."
+
+print "In dog years you are ", (age*7), "years old"
+
