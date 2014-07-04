16fe73af8be5c49dbf9fb1fa2cd68e8198a4ae60
Jerry O'Donnell <gerald-odonnell7@users.noreply.github.com>
2014-07-03 21:15:56 -0400

    Create animalDriver.py

---
+++ animalDriver.py
@@ -0,0 +1,12 @@
+from animal import Animal
+
+puppy = Animal("Rover", 36, 48, "Brown", "Bark")
+puppy.eat()
+puppy.sleep()
+puppy.run()
+puppy.displayAnimalCount()
+
+fish = Animal("Bubbles", 2, 1, "Gold", "blub")
+fish.eat()
+fish.sleep()
+fish.displayAnimalCount()
