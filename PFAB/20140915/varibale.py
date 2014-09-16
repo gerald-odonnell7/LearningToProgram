d53543de38ef992ab9fbbe84d4fa988b63bfdc6e
Jerry O'Donnell <gerald-odonnell7@users.noreply.github.com>
2014-09-15 20:42:17 -0400

    Create varibale.py

---
+++ PFAB/20140915/varibale.py
@@ -0,0 +1,15 @@
+import math
+* / + - %
+
+
+rate=.08
+loan=6000
+print(loan)
+print(rate/12 * loan);
+
+print(43%7)
+
+
+firstname = 'Jerry'
+lastname = "O'Donnell"
+print(firstname + " " + lastname)
