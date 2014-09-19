63cbfd11c7978a715030ea1d3df8bcf3bf22daed
Jerry O'Donnell <gerald-odonnell7@users.noreply.github.com>
2014-09-18 20:46:40 -0400

    Create vote.py

---
+++ PFAB/2014-09-18/vote.py
@@ -0,0 +1,18 @@
+age = input("How old are you?")
+citizen = raw_input("Are you a citizen [Y/N]?")
+
+if(age>=18 and citizen=="Y"):
+	print "You are eligible to vote"
+	print "Go vote"
+else:
+	print "You are not eligible to vote"
+
+print "Thank you for your input"
+
+#comparisons
+#equal == not just =
+#greater than >
+#greater than and equal >=
+#less than <
+#less than and equal <=
+#not equal <> or !=
