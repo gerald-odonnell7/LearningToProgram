ff2014f4fe5993d8a0aebb7882f2df1e655f68c3
Jerry O'Donnell <gerald-odonnell7@users.noreply.github.com>
2014-09-22 20:36:15 -0400

    Create teams.py

---
+++ PFAB/2014-09-22/teams.py
@@ -0,0 +1,12 @@
+teams = ['Yankees', 'Dodgers', 'Mets', 'Giants', 'Orioles', 'Angels', 'Reds', 'Red Soxs', 'White Soxs']
+
+x = 0
+#LEN = counts the items
+while x < len(teams):
+	print teams[x]
+	x = x + 1
+
+
+#FOR singularvariable IN listname:
+for team in teams:
+	print "The teams is", team
