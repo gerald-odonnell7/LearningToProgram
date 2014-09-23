f24d9b5a221b4dce635bffb7ff7e9de9c3938ea4
Jerry O'Donnell <gerald-odonnell7@users.noreply.github.com>
2014-09-22 20:36:44 -0400

    Create tupals.py

---
+++ PFAB/2014-09-22/tupals.py
@@ -0,0 +1,11 @@
+subjects = ('physics', 'chemistry', 'algebra', 'gym', 'biology')
+print subjects
+print 'My favorite class is ', subjects[3]
+
+#Tupals cannot change values
+#subjects[3] = 'english'
+#print subjects
+
+classrooms = (101, 107, 210, 0, 109)
+print 'Highest classroom number', max(classrooms)
+print 'Lowest classroom number', min(classrooms)
